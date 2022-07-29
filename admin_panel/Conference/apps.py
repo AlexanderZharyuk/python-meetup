ccfd58dd4e8cb6f075eea0d0162d57c0c8364bec
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-29 15:39:07 +0300

    Change paths to absolute

--- admin_panel/Conference/apps.py
+++ admin_panel/Conference/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ConferenceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Conference'
+    name = 'admin_panel.Conference'
--- admin_panel/Meetup/settings.py
+++ admin_panel/Meetup/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Conference',
+    'admin_panel.Conference',
 ]
 
 MIDDLEWARE = [
