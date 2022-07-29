d344aaaa26f141982fb8decd12d95fc212bd4645
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-29 14:40:03 +0300

    change settings path

--- admin_panel/Meetup/wsgi.py
+++ admin_panel/Meetup/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Meetup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_panel.Meetup.settings')
 
 application = get_wsgi_application()
