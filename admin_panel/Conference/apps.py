4438b3e9c65bcca1cc2db8e1052655ac9ce35966
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-29 19:06:33 +0300

    Config django for ORM usage

--- admin_panel/Conference/apps.py
+++ admin_panel/Conference/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ConferenceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_panel.Conference'
+
+    if hasattr(settings, 'USING_TG_BOT_SETTINGS') and settings.USING_TG_BOT_SETTINGS:
+        name = 'admin_panel.Conference'
+    else:
+        name = "Conference"
